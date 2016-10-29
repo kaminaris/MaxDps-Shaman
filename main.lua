dae1d726cc9317ea664633993f571e1e3e68df2f
Pawel <zacdbg@gmail.com>
2016-10-29 17:23:12 +0200

    v1.5 - enh support

--- main.lua
+++ main.lua
@@ -14,6 +14,22 @@ local _FireElemental = 198067;
 local _Stormkeeper = 205495;
 local _ElementalMastery = 16166;
 
+-- enh
+local _Boulderfist = 201897;
+local _Landslide = 197992;
+local _Hailstorm = 210853;
+local _Frostbrand = 196834;
+local _CrashLightning = 187874;
+local _Flametongue = 193796;
+local _Stormstrike = 17364;
+local _Stormbringer = 201845;
+local _FeralSpirit = 51533;
+local _AlphaWolf = 198434;
+local _DoomWinds = 204945;
+local _CrashingStorm = 192246;
+local _LavaLash = 60103;
+local _LightningBoltEnh = 187837;
+
 -- totems
 local _SearingTotem = 3599;
 local _StormElementalTotem = 152256;
@@ -21,23 +37,20 @@ local _FireElementalTotem = 2894;
 
 -- auras
 local _LavaSurge = 77762;
-local _ElementalFusion = 157174;
-local _EnhancedChainLightning = 157766;
 
 -- talents
 local _isAscendance = false;
+local _isHailstorm = false;
+local _isCrashingStorm = false;
 local _isElementalMastery = false;
-
--- Flags
-
-local _FlagAscendance = false;
-local _FlagLS = false;
 
 ----------------------------------------------
 -- Pre enable, checking talents
 ----------------------------------------------
 TDDps_Shaman_CheckTalents = function()
 	_isAscendance = TD_TalentEnabled('Ascendance');
+	_isHailstorm = TD_TalentEnabled('Hailstorm');
+	_isCrashingStorm = TD_TalentEnabled('Crashing Storm');
 	_isElementalMastery = TD_TalentEnabled('Elemental Mastery');
 end
 
@@ -46,10 +59,13 @@ end
 ----------------------------------------------
 function TDDps_Shaman_EnableAddon(mode)
 	mode = mode or 1;
-	_TD['DPS_Description'] = 'TD Shaman DPS supports: Elemental';
+	_TD['DPS_Description'] = 'TD Shaman DPS supports: Elemental, Enhancement';
 	_TD['DPS_OnEnable'] = TDDps_Shaman_CheckTalents;
 	if mode == 1 then
-		_TD['DPS_NextSpell'] = TDDps_Shaman_Elemental
+		_TD['DPS_NextSpell'] = TDDps_Shaman_Elemental;
+	end;
+	if mode == 2 then
+		_TD['DPS_NextSpell'] = TDDps_Shaman_Enhancement;
 	end;
 	TDDps_EnableAddon();
 end
@@ -101,6 +117,67 @@ TDDps_Shaman_Elemental = function()
 	return _LightningBolt;
 end
 
+----------------------------------------------
+-- Main rotation: Enhancement
+----------------------------------------------
+TDDps_Shaman_Enhancement = function()
+	local timeShift, currentSpell, gcd = TD_EndCast();
+
+	local mael = UnitPower('player', SPELL_POWER_MAELSTORM);
+
+	local bfCd, bfCharges = TD_SpellCharges(_Boulderfist, timeShift);
+	local ssCd = TD_SpellAvailable(_Stormstrike, timeShift);
+	local fs = TD_SpellAvailable(_FeralSpirit, timeShift);
+	local dw = TD_SpellAvailable(_DoomWinds, timeShift);
+	local cl = TD_SpellAvailable(_CrashLightning, timeShift);
+	local ftCd = TD_SpellAvailable(_Flametongue, timeShift);
+
+	local bf = TD_Aura(_Boulderfist, timeShift + 2);
+	local ls = TD_Aura(_Landslide, timeShift + 2);
+	local fb = TD_Aura(_Frostbrand, timeShift + 4);
+	local ft = TD_Aura(_Flametongue, timeShift + 4);
+
+	TDButton_GlowCooldown(_FeralSpirit, fs);
+	TDButton_GlowCooldown(_DoomWinds, dw);
+
+	if (not bf or not ls) and bfCd then
+		return _Boulderfist;
+	end
+
+	if _isHailstorm and not fb then
+		return _Frostbrand;
+	end
+
+	if not ft and ftCd then
+		return _Flametongue;
+	end
+
+	if ssCd then
+		return _Stormstrike;
+	end
+
+	if mael < 130 and bfCharges > 1 then
+		return _Boulderfist;
+	end
+
+	if _isCrashingStorm and cl then
+		return _CrashLightning;
+	end
+
+	if mael > 110 then
+		return _LavaLash;
+	end
+
+	if bfCharges > 0 then
+		return _Boulderfist;
+	end
+
+	if ftCd then
+		return _Flametongue;
+	end
+
+	return _LightningBoltEnh;
+end
 ----------------------------------------------
 -- Fire totem name and expiration
 ----------------------------------------------
