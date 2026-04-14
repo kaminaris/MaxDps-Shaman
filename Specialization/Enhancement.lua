939d6ae8b2a0875a58750c7a45342ed9c4413faf
tbkizle <tbkizle@gmail.com>
2026-04-14 03:05:30 -0400

    Update All Retail

--- Specialization/Enhancement.lua
+++ Specialization/Enhancement.lua
@@ -3,6 +3,9 @@ local Shaman = addonTable.Shaman
 local MaxDps = _G.MaxDps
 if not MaxDps then return end
 
+local GetItemCooldown = C_Item.GetItemCooldown
+local usedTrinkets = {}
+
 local Enhancement = {}
 
 function Shaman:Enhancement()
